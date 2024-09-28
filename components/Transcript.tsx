652ca11b89f4c07cccd7740a03107a97e4c723d7
Luan Tran <developedbyluan@gmail.com>
2024-09-28 18:06:09 +0700

    Move arrayToObjectsArray to Transcript

--- components/Transcript.tsx
+++ components/Transcript.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 
 export default function Transcript(props: { text: string }) {
-    React.useEffect(() => {
-        const lines = props.text.split("\n\n")
-        console.log(lines)
-    }, [props.text])
+  function arrayToObjectsArray(array: string[]) {
+    const lineObjectsArray = array.map((line) => {
+      const parts = line.split("---");
 
-    const lineElements = props.text.split("\n\n").map((line, index) => {    
-        return <div key={index}>{line}</div>
-    })
+      if (parts.length !== 5) {
+        const [text] = parts;
+        return { text };
+      }
+
+      const [text, ipa, translation, imgUrlandAlt, type] = parts;
+      const [imgUrl, alt] = imgUrlandAlt.split("|");
+      return {
+        text,
+        ipa,
+        translation,
+        imgUrl,
+        alt,
+        type,
+      };
+    });
+
+    return lineObjectsArray;
+  }
+
+  const lineElements = arrayToObjectsArray(props.text.split("\n\n")).map(
+    (line, index) => {
+      return <div key={index}>{line.text}</div>;
+    }
+  );
   return <div>{lineElements}</div>;
 }
