34ff49592dd1f9058042813939e82629b85e44de
Luan Tran <developedbyluan@gmail.com>
2024-09-28 18:12:34 +0700

    Revert "Pass state text to Transcript component as prop and render its content"
    This reverts commit b8d94f20a7326eb6dbfd339ef6a4e8d6064df784.

--- components/Transcript.tsx
+++ components/Transcript.tsx
@@ -1,33 +1,7 @@
-import React from "react";
-
 export default function Transcript(props: { text: string }) {
-  function arrayToObjectsArray(array: string[]) {
-    const lineObjectsArray = array.map((line) => {
-      const parts = line.split("---");
-
-      if (parts.length !== 5) {
-        const [text] = parts;
-        return { text };
-      }
-
-      const [text, ipa, translation, imgUrlandAlt, type] = parts;
-      const [imgUrl, alt] = imgUrlandAlt.split("|");
-      return {
-        text,
-        ipa,
-        translation,
-        imgUrl,
-        alt,
-        type,
-      };
-    });
-
-    return lineObjectsArray;
-  }
-
-  const lineElements = arrayToObjectsArray(props.text.split("\n\n")).map(
+  const lineElements = props.text.split("\n\n").map(
     (line, index) => {
-      return <div key={index}>{line.text}</div>;
+      return <div key={index}>{line}</div>;
     }
   );
   return <div>{lineElements}</div>;
