70dbd197be76b211ed95aa297ce4ae3bd736842e
Luan Tran <developedbyluan@gmail.com>
2024-10-01 09:28:33 +0700

    Feat: Scroll into view the last element of Subtitles

--- components/Subtitles.tsx
+++ components/Subtitles.tsx
@@ -1,21 +1,30 @@
 import type { Transcript as TranscriptType } from "@/types/Transcript";
 import Image from "next/image";
+import React from "react";
 
 export default function Subtitles(props: {
   subtitlesArray: TranscriptType[] | Partial<TranscriptType>[];
   timestampsArray: number[];
 }) {
+  const lastElementRef = React.useRef<HTMLElement | null>(null)
+
   const subtitlesElements = props.subtitlesArray.map((line, index) => {
+    const callbackRef = (e: HTMLDivElement) => {
+      if (index === props.subtitlesArray.length - 1) {
+        lastElementRef.current = e 
+      }
+      console.log(lastElementRef.current)
+    }
     if (Object.keys(line).length !== 7) {
       return (
-        <div key={crypto.randomUUID()}>
+        <div key={crypto.randomUUID()} ref={callbackRef}>
           <p>{index + 1}</p>
           <p>{line.text}</p>
         </div>
       );
     }
     return (
-      <div key={crypto.randomUUID()}>
+      <div key={crypto.randomUUID()} ref={callbackRef}>
         <p>{index + 1}</p>
         <p>
           {props.timestampsArray?.[index - 1] || 0} {"-->"}{" "}
@@ -46,6 +55,15 @@ export default function Subtitles(props: {
       </div>
     );
   });
+
+  React.useEffect(() => {
+    lastElementRef.current?.scrollIntoView(
+      {
+        block: "start",
+        behavior: "smooth"
+      }
+    )
+  }, [props.subtitlesArray])
   return (
     <div className="flex flex-col gap-7 bg-neutral-50 p-4">
       {subtitlesElements}
