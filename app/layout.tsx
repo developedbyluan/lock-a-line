fa7438d5930ca1d80ed4991781ddc10d0b160543
Luan Tran <developedbyluan@gmail.com>
2024-10-03 20:39:46 +0700

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ export const metadata: Metadata = {
   title: "Log a Line - Generate Subtitles Manually",
   description: "Generate subtitles for your mini stories",
   keywords: ["subtitle", "story", "generate"],
+  icons: {
+    icon: "favicon.ico"
+  }
 };
 
 export default function RootLayout({
